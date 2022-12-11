cfbd0c62bc85790e19fcc236927366664a869109
Jayromberg Lima Santos <jayro@pop-os.localdomain>
2022-12-10 21:51:57 -0300

    refactor: config para providers

---
+++ src/infra/providers/database.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import { Sequelize } from 'sequelize-typescript';
+
+export const databaseProviders = [
+  {
+    provide: 'SEQUELIZE',
+    useFactory: async () => {
+      const sequelize = new Sequelize({
+        dialect: 'mysql',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DATABASE,
+      });
+      sequelize.addModels([]);
+      await sequelize.sync();
+      return sequelize;
+    },
+  },
+];
